599906168ab6f57063b1ec69b8b3e82e92d93f90
xxh <tang2988@qq.com>
2019-02-15 00:07:10 +0800

    首次提交 用户端项目

---
+++ src/main/java/com/taotaoke/portal/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.taotaoke.portal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/index")
+	public String IndexMethod(){
+		return "index";
+	}
+
+}
